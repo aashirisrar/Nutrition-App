230eb88679dd17f5c5d7611fb10882f0d0386c87
Aashir Israr <aashirisrar968@gmail.com>
2023-01-07 23:22:05 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get("/search", (req, res) => {
+    const options = {
+        method: "GET",
+        url: "https://nutritionix-api.p.rapidapi.com/v1_1/search/milk",
+        params: { fields: "item_name,nf_calories,nf_total_fat" },
+        headers: {
+            "X-RapidAPI-Key": "b8eb4c64d1msh82f4f5538e5065ap1d2b92jsn7113e96b5efb",
+            "X-RapidAPI-Host": "nutritionix-api.p.rapidapi.com",
+        },
+    };
+
+    axios
+        .request(options)
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port} at http://localhost:3000/`);
+});
